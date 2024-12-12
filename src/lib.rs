b634cf26e3572832f6527133cc5c91ec15e23291
Solandy <solandy.consulting@gmail.com>
2024-12-12 17:37:46 +0100

    commit for a new hash

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ use pinocchio::{
     lazy_entrypoint::{ InstructionContext}, ProgramResult
   };
   use pinocchio::lazy_entrypoint;
+use solana_program::msg;
 
 lazy_entrypoint!(process_instruction);
 
 pub fn process_instruction(
 _context: InstructionContext,
 ) -> ProgramResult {
+    msg!("give me a different hash please!");
     Ok(())
 }
